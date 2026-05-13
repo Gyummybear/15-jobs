a46038e260a845f44b63ffd75ad6f1e19d0a47e0
Gyummybear <uhg02144@naver.com>
2026-05-13 17:38:29 +0900

    260513 채용사이트 크롤링

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, send_file
 from scrapper import search_incruit
 from file import save_to_csv
+# from scrapper import search_incruit, search_saramin # 함수 추가 임포트
 
 app = Flask(__name__)
 
@@ -24,6 +25,18 @@ def file():
     save_to_csv(jobs)
     return send_file("downloads.csv", as_attachment=True)
 
+# @app.route("/search")
+# def search():
+#     keyword = request.args.get("keyword")
+
+#     # 두 사이트의 결과를 모두 가져와서 합칩니다.
+#     incruit_jobs = search_incruit(keyword, 1)
+#     saramin_jobs = search_saramin(keyword, 1)
+    
+#     # 리스트 두 개를 합쳐서 하나의 jobs 리스트로 만듭니다.
+#     all_jobs = incruit_jobs + saramin_jobs
+
+#     return render_template("search.html", jobs=enumerate(all_jobs), keyword=keyword)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- scrapper.py
+++ scrapper.py
@@ -36,4 +36,41 @@ def search_incruit(keyword, pages):
             
     return jobs
 
+def search_saramin(keyword, pages):
+    jobs = []
+    # 사람인은 차단 방지를 위해 headers가 꼭 필요합니다. (살짝 추가)
+    headers = {"User-Agent": "Mozilla/5.0"}
 
+    for i in range(pages):
+        # 인크루트는 i * 30이었지만, 사람인은 그냥 페이지 번호를 씁니다.
+        url = f"https://www.saramin.co.kr/zf_user/search/recruit?searchword={keyword}&recruitPage={i+1}"
+        response = requests.get(url, headers=headers)
+        soup = BeautifulSoup(response.text, "html.parser")
+
+        # [변경포인트 1] li.c_col 대신 div.item_recruit를 찾습니다.
+        lis = soup.find_all("div", class_="item_recruit")
+
+        for li in lis:
+            # [변경포인트 2] 클래스 이름을 사람인에 맞게 수정합니다.
+            # 인크루트: cpname -> 사람인: corp_name
+            company = li.find("div", class_="corp_name").find("a").text.strip()
+            
+            # 인크루트: cl_top -> 사람인: job_tit
+            title_tag = li.find("div", class_="job_tit").find("a")
+            title = title_tag.text.strip()
+            
+            # 인크루트: 0번째 span -> 사람인: 0번째 span (동일)
+            location = li.find("div", class_="job_condition").find_all("span")[0].text.strip()
+            
+            # 링크도 태그 안에서 가져오기 (도메인 추가)
+            link = "https://www.saramin.co.kr" + title_tag["href"]
+        
+            job_data = {
+                "company" : company,
+                "title" : title,
+                "location": location,
+                "link" : link
+            }
+            jobs.append(job_data)
+            
+    return jobs
